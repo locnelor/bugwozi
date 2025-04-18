98c69c2f145a066d3f4966d914c147b152be21bb
locnelor <locnelor@icloud.com>
2025-04-18 21:51:52 +0800

    i

--- apps/api/libs/prisma/src/entity/sys/sys.role.entity.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SysRoleEntity = void 0;
-var SysRoleEntity = /** @class */ (function () {
-    function SysRoleEntity() {
-    }
-    return SysRoleEntity;
-}());
-exports.SysRoleEntity = SysRoleEntity;
