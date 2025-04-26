d8f3aa6e7e9868cd6cd8f472239b544a405c945a
locnelor <locnelor@icloud.com>
2025-04-26 14:48:34 +0800

    i

--- packages/database/generated/client/edge.js
+++ packages/database/generated/client/edge.js
@@ -214,7 +214,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "/Users/wangwenhui/files/code/bugwozi/packages/database/generated/client",
+      "value": "/Users/locnelor/files/code/bugwozi/packages/database/generated/client",
       "fromEnvVar": null
     },
     "config": {
@@ -228,7 +228,7 @@ const config = {
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "/Users/wangwenhui/files/code/bugwozi/packages/database/prisma/schema.prisma",
+    "sourceFilePath": "/Users/locnelor/files/code/bugwozi/packages/database/prisma/schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
