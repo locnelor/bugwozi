a49f316231ab2482b014cd40b229865879e66f68
locnelor <locnelor@icloud.com>
2025-04-30 10:54:01 +0800

    book

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -13,6 +13,7 @@ import { DashboardModule } from './dashboard/dashboard.module';
 import { RedisCache, WeChatModule } from '@app/wechat';
 import { WxModule } from './wx/wx.module';
 import { BookkeepingModule } from './bookkeeping/bookkeeping.module';
+import { BookModule } from './book/book.module';
 
 @Module({
   imports: [
@@ -88,6 +89,7 @@ import { BookkeepingModule } from './bookkeeping/bookkeeping.module';
     DashboardModule,
     WxModule,
     BookkeepingModule,
+    BookModule,
   ],
   controllers: [AppController],
   providers: [AppService],
