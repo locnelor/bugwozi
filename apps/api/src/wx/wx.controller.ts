a91d811c8b7ae3fab25b4c69d1917918b2a21aa8
locnelor <locnelor@icloud.com>
2025-05-01 22:50:44 +0800

    fix

--- apps/api/src/wx/wx.controller.ts
+++ apps/api/src/wx/wx.controller.ts
@@ -4,6 +4,7 @@ import { Response } from 'express';
 import { XMLBuilder } from 'fast-xml-parser';
 import { createHash } from 'crypto';
 import { ConfigService } from '@nestjs/config';
+import { AuthService } from 'src/system/auth/auth.service';
 interface IWxMessageXmlData {
   /** 开发者微信号 e.g. `gh_019087f88815`*/
   ToUserName: string;
@@ -14,7 +15,7 @@ interface IWxMessageXmlData {
   /** 消息类型，此处为 `event` */
   MsgType: string;
   /** 事件类型，subscribe(订阅)、unsubscribe(取消订阅) */
-  Event: 'subscribe' | 'unsubscribe';
+  Event: 'subscribe' | 'unsubscribe' | "SCAN";
   /** 事件KEY值，目前无用 */
   EventKey: string;
 
@@ -26,7 +27,8 @@ interface IWxMessageXmlData {
 @Controller('wechat')
 export class WxController {
   constructor(
-    private readonly configService: ConfigService
+    private readonly configService: ConfigService,
+    private readonly authService: AuthService
   ) { }
 
   @Get("handle")
@@ -49,6 +51,14 @@ export class WxController {
   }
 
 
+  //   @web/api:dev:   ToUserName: 'gh_bdb21a973b5c',
+  //   @web/api:dev:   FromUserName: 'oWZyw6edoc0XygKjECXTem--pmJI',
+  //   @web/api:dev:   CreateTime: '1746110390',
+  //   @web/api:dev:   MsgType: 'event',
+  //   @web/api:dev:   Event: 'SCAN',
+  //   @web/api:dev:   EventKey: 'b3ff5e1e-1b33-45d3-96a1-b355ab703b04',
+  //   @web/api:dev:   Ticket: 'gQFt7zwAAAAAAAAAAS5odHRwOi8vd2VpeGluLnFxLmNvbS9xLzAybngyd0FicHNlTUYxYWw4anhFY1cAAgRphxNoAwQsAQAA',
+  //   @web/api:dev:   Encrypt: 'Njj7X+wb7LcWYpXdLQUZuzyLnFR1nBMCziZ9kYdfwLVDxareNOF7ltr7CKLE8M//fSeYh1HyTWetu/SecZZhLQQn9S7OsHfy0VJzqcwgNQkoc5PYkELR0Ag6Q9U2XoD6dXCT4WNEMz3/KgqLkdT0+E5r36IObyHAEcIl/O+2nEl/kr8SjgvnctnSN29ibVWHPqEML7G4bfnjGE1sdWRb9xYuA1zCNMJfPloO4eNDSVaMZYDKHZmi3Kijp25Xd6By7Z+jaeD78hzrQVC+jgFlC0HYBzX4gG/y5vMSa4ubeBn/45uL9/O9S2dwAvaivM5yGeGvVW6zcWL521Pr61baKfC1+Ts8Bp8EClG3qNf7QKrRjuRGLq3Hg8+BLWQa9/GzmjmJjIu6NqBW/xX0RNkgndlSV37zW8wMbVM3tu8VCC5TKlW83CopvKC8u6zaeWWxiiUOalLnIaEP/y89nefgxsPY6iDZc/8nHQolk23Gus7spG7LFgewxB5JaMErAn55KRHJS1DjB7TaISu2JNidtbEqYVH22i4nffPrzXeqxjNxPBose6MLMOty2K6Ll94j9aVSjhS7rRtAzf9fGaayftQi2/dGR3peGgb07aRZQM1liwIe0jT4m/ECNQvxjxIY'
 
   @Post("handle")
   async handle(
@@ -56,15 +66,16 @@ export class WxController {
     @Body('xml') msg: IWxMessageXmlData,
   ) {
     console.log(msg)
-    if (msg.Event === "subscribe") {
-      const openid = msg.FromUserName;
-      const time = msg.CreateTime;
-      const Ticket = msg.Ticket
+    const openid = msg.FromUserName;
+    if (msg.Event === "SCAN") {
+      // const time = msg.CreateTime;
+      // const Ticket = msg.Ticket
       const EventKey = msg.EventKey;
-      console.log(openid)
-      console.log(time)
-      console.log(Ticket);
-      console.log(EventKey)
+      this.authService.setQrCode(EventKey, openid);
+      // console.log(openid)
+      // console.log(time)
+      // console.log(Ticket);
+      // console.log(EventKey)
 
       res.set('Content-Type', 'application/xml');
       res.send('');
--- apps/api/src/wx/wx.module.ts
+++ apps/api/src/wx/wx.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { WxService } from './wx.service';
 import { WxController } from './wx.controller';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from 'src/system/auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule
+    ConfigModule,
+    AuthModule
   ],
   providers: [WxService],
   controllers: [WxController],
