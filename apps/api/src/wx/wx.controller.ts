40d240dc919f1f69768f027e077672934d0bcda1
locnelor <locnelor@icloud.com>
2025-05-01 18:52:08 +0800

    wechat

--- apps/api/src/wx/wx.controller.ts
+++ apps/api/src/wx/wx.controller.ts
@@ -1,6 +1,6 @@
 import { MessageCrypto, WeChatService } from '@app/wechat';
 import { Body, Controller, Post, Req, Res } from '@nestjs/common';
-import { xml } from 'body-parser';
+import { raw, xml } from 'body-parser';
 import { Request, Response } from 'express';
 import { XMLParser } from 'fast-xml-parser';
 
@@ -32,22 +32,29 @@ export class WxController {
       }
     }
 
-    // let decrypt = '';
-    // let success = false;
-    // const aesKey = MessageCrypto.getAESKey(signature || '');
-    // const iv = MessageCrypto.getAESKeyIV(aesKey);
-    // const parser = new XMLParser();
-    // const xml = parser.parse(rawBody.toString());
-    // const encryptMessage = xml.Encrypt;
-    // const data = MessageCrypto.decrypt(aesKey, iv, encryptMessage);
+    console.log(timestamp)
+    console.log(nonce)
+    console.log(rawBody)
+    console.log(signature)
 
 
-    // if (timestamp && nonce && signature && rawBody) {
-    //   decrypt = this.decryptMessage(signature as string, timestamp as string, nonce as string, rawBody.toString());
-    //   success = true;
-    // } else {
-    //   throw new Error('message params incorrect');
-    // }
+    let decrypt = '';
+    let success = false;
+    const aesKey = MessageCrypto.getAESKey(signature || '');
+    const iv = MessageCrypto.getAESKeyIV(aesKey);
+    const parser = new XMLParser();
+    const xml = parser.parse(rawBody.toString());
+    const encryptMessage = xml.Encrypt;
+    const data = MessageCrypto.decrypt(aesKey, iv, encryptMessage);
+    console.log(data)
+
+
+    if (timestamp && nonce && signature && rawBody) {
+      // decrypt = this.decryptMessage(signature as string, timestamp as string, nonce as string, rawBody.toString());
+      success = true;
+    } else {
+      throw new Error('message params incorrect');
+    }
     const text = await this.wechatService.messagePushExpressHandler(req);
     console.log(text);
     return ""
