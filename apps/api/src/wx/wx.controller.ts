9cdaf71963f4659501c39444a8d101ee9fede002
locnelor <locnelor@icloud.com>
2025-05-01 17:12:08 +0800

    package

--- apps/api/src/wx/wx.controller.ts
+++ apps/api/src/wx/wx.controller.ts
@@ -8,9 +8,20 @@ export class WxController {
   ) { }
 
   @Post("handle")
-  handle(
+  async handle(
     @Body() body
   ) {
     console.log(body)
+    const a = await this.wechatService
+    return `
+    
+<xml>
+  <ToUserName><![CDATA[toUser]]></ToUserName>
+  <FromUserName><![CDATA[fromUser]]></FromUserName>
+  <CreateTime>12345678</CreateTime>
+  <MsgType><![CDATA[text]]></MsgType>
+  <Content><![CDATA[你好]]></Content>
+</xml>
+    `
   }
 }
